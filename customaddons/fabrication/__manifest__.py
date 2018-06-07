79c0b3457dc9d172072e5fad4a104db04aa58ae0
sysall <sall.momo2@gmail.com>
2018-06-07 14:57:56 +0000

    Task 68 : hide the menu unbuild orders: DONE #sy_sall

---
+++ customaddons/fabrication/__manifest__.py
@@ -0,0 +1,9 @@
+{
+	'name' : "Fabrication",
+	'version' : "1.0",
+	'description' : 'this module is design for hiding menu in manufactoring',
+	'author' : "SySall",
+	'depends' : ['base','mrp'],
+	'data' : ['fabrication_view.xml'],
+	'installable' : True,
+}
